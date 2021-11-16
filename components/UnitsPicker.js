8a0cbf2950af0b32209077416417a5a4d4da96ff
Ciurtea Virgil <37183631+virgilotto@users.noreply.github.com>
2021-11-16 20:32:34 +0200

    initial commit

---
+++ components/UnitsPicker.js
@@ -0,0 +1,36 @@
+import { Picker } from '@react-native-community/picker'
+import React from 'react'
+import { View, Text, StyleSheet, Platform } from 'react-native'
+
+export default function UnitsPicker({ unitsSystem, setUnitsSystem }) {
+    return (
+        <View style={styles.unitsSystem}>
+            <Picker 
+                selectedValue={unitsSystem} 
+                onValueChange={(item) => setUnitsSystem(item)} 
+                mode='dropdown' 
+                itemStyle={{ fontSize: 12 }}
+            >
+                <Picker.Item label='C' value='metric' />
+                <Picker.Item label='F' value='imperial' />
+            </Picker>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    unitsSystem: {
+        position: 'absolute',
+        ...Platform.select({
+            ios: {
+                top: -30,
+            },
+            android: {
+                top: 30,
+            }
+        }),
+        left: 20,
+        height: 50,
+        width: 100
+    }
+})
